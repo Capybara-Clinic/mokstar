145697131478ccf2c57b3cbf74ed1b5035ace9bc
kmgyu <lidersy961@gmail.com>
2025-04-15 01:40:56 +0900

    fix: login page error fixed - useHistory is React Router v5<= hooks. changed to useNavigate

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,3 +1,28 @@
+import { useState, useContext, useEffect } from "react";
+import { useNavigate  } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+// near 1:20:00 part, read that
+
 export default function Login() {
-    return <p>this is login page</p>;
+    const navigate = useNavigate ();
+    const {firebase} = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => {};
+
+    useEffect(() => {
+        document.title = 'Login - Mokstagram';
+    }, []) 
+
+    return ( // tailwind?
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <p>I have no idea!</p>
+        </div>
+    );
 }
