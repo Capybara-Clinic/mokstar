ab2ed1fa301f57707359189f6537a6cd8db6fa55
kmgyu <lidersy961@gmail.com>
2025-04-20 21:49:49 +0900

    feat: sidebar

--- front-end/src/services/firebase.js
+++ front-end/src/services/firebase.js
@@ -10,3 +10,18 @@ export async function doesUsernameExist(username) {
   const querySnapshot = await getDocs(q);
   return querySnapshot.docs.length > 0;
 }
+
+export async function getUserByUserId(userId) {
+  const q = query(
+    collection(db, 'users'),
+    where('userId', '==', userId)
+  );
+  const snapshot = await getDocs(q);
+
+  const user = snapshot.docs.map((item) => ({
+    ... item.data(),
+    docId: item.id
+  }));
+
+  return user;
+}
