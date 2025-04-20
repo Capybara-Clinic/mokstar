8be41006453897897b94206757949f92cb954e93
kmgyu <lidersy961@gmail.com>
2025-04-20 18:36:43 +0900

    chore: style.css apply

--- front-end/src/pages/not-found.js
+++ front-end/src/pages/not-found.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import '../styles/app.css';
 
 export default function NotFound() {
     useEffect(() => {
@@ -7,7 +8,7 @@ export default function NotFound() {
 
     return (
         <div className="bg-gray-background">
-            <div className="mx-auth max-w-screen-lg">
+            <div className="mx-auto max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
         </div>
