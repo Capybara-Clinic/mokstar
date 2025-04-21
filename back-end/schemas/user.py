7a9c23d5a5ef192a5cccbcafe2c29a5c27636c4c
mksjy <160725687+mksjy@users.noreply.github.com>
2025-04-21 21:40:40 +0900

    Feat: define User schema for serialization and exclude password field

---
+++ back-end/schemas/user.py
@@ -0,0 +1,10 @@
+from extensions import ma
+from models.user import User
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        exclude = ("pw",)
+
+user_schema = UserSchema()
