59d819713f524275c9c55ca867f7ff4481c68868
mksjy <160725687+mksjy@users.noreply.github.com>
2025-04-22 15:12:28 +0900

    Refactor UserSchema: use auto_field instead of empty Meta-only schema

--- back-end/schemas/user.py
+++ back-end/schemas/user.py
@@ -1,10 +1,12 @@
 from extensions import ma
 from models.user import User
 
-class UserSchema(ma.SQLAlchemyAutoSchema):
+class UserSchema(ma.SQLAlchemySchema):
     class Meta:
         model = User
         load_instance = True
-        exclude = ("pw",)
 
-user_schema = UserSchema()
+    user_id = ma.auto_field()
+    pw = ma.auto_field(load_only=True)
+    email = ma.auto_field()
+    nickname = ma.auto_field()
