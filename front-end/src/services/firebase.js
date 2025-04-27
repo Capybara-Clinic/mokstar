a1acea14ee1b1fa33f9cf4c9263945297d2f41ba
kmgyu <lidersy961@gmail.com>
2025-04-27 18:44:54 +0900

    [feat] suggestion profiles

--- front-end/src/services/firebase.js
+++ front-end/src/services/firebase.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import { db } from '../lib/firebase'; // db = getFirestore(firebaseApp) 해야 함
 
 export async function doesUsernameExist(username) {
@@ -24,4 +24,20 @@ export async function getUserByUserId(userId) {
   }));
 
   return user;
+}
+
+export async function getSuggestedProfiles(userId, following = []) {
+  const q = query(
+    collection(db, 'users'),
+    limit(10)
+  );
+
+  const snapshot = await getDocs(q);
+  return snapshot.docs.map((user) => ({
+    ... user.data(),
+    docId: user.id
+  }))
+  .filter((profile) =>
+  profile.userId !== userId && !following.includes(profile.userId));
+  // const result = await coll
 }
