3bae03caeef2875c722a93704b537a818d178a0d
kmgyu <lidersy961@gmail.com>
2025-04-27 19:14:57 +0900

    [feat] Timeline photos display

--- front-end/src/services/firebase.js
+++ front-end/src/services/firebase.js
@@ -41,3 +41,28 @@ export async function getSuggestedProfiles(userId, following = []) {
   profile.userId !== userId && !following.includes(profile.userId));
   // const result = await coll
 }
+
+export async function getPhotos(userId, following) {
+  const q = query(
+    collection(db, 'photos'),
+    where('userid', 'in', following)
+  );
+  const snapshot = await getDocs(q);
+  const photos = snapshot.docs.map((item) => ({
+    ... item.data(),
+    docId: item.id
+  }));
+
+  const photosWithUserDetails = await Promise.all(
+    photos.map(async (photo) => {
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true;
+      }
+      const user = await getUserByUserId(photo.userId);
+      const {username} = user[0];
+      return {username, ...photo, userLikedPhoto };
+    })
+  );
+  return photosWithUserDetails;
+}
